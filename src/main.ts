45740f2b6dd0371f02893957ac1c36176ebcb8e1
WEISHAOY\16047 <1604705673@qq.com>
2024-03-19 17:39:07 +0800

    ✨ feat: 增加nprogress 顶部进度条

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+
 import { createRouter, createWebHistory } from 'vue-router/auto'
+
+import NProgressPlugin from './utils/nporgress'
+
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -11,8 +15,20 @@ import 'uno.css'
 import './iconFont'
 
 const app = createApp(App)
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 })
+
+// beforeEach路由切换之前触发
+router.beforeEach(() => {
+  NProgressPlugin.start() // 开始进度条
+})
+
+// afterEach路由切换之后触发
+router.afterEach(() => {
+  NProgressPlugin.close() // 结束进度条
+})
+
 app.use(router)
 app.mount('#app')
