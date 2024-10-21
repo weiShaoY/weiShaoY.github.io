51b3cc198c31198a3c41aa5ba1a6aa0bfada7f36
weiShaoY_ <1604705673@qq.com>
2024-10-22 02:29:18 +0800

    chore: 🔧全局组件文件夹结构调整

--- components.d.ts
+++ components.d.ts
@@ -7,16 +7,16 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    Chart: typeof import('./src/components/Chart.vue')['default']
+    Chart: typeof import('./src/components/Chart/index.vue')['default']
     'Chart_副本': typeof import('./src/components/Chart_副本.vue')['default']
     Checkbox: typeof import('./src/components/Checkbox.vue')['default']
     Echarts: typeof import('./src/components/echarts/index.vue')['default']
     PageSwitch: typeof import('./src/components/TheHeader/components/page-switch.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    SvgIcon: typeof import('./src/components/SvgIcon.vue')['default']
+    SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
     TheCounter: typeof import('./src/components/TheCounter.vue')['default']
-    TheFooter: typeof import('./src/components/TheFooter.vue')['default']
+    TheFooter: typeof import('./src/components/TheFooter/index.vue')['default']
     TheHeader: typeof import('./src/components/TheHeader/index.vue')['default']
     ThemeSwitch: typeof import('./src/components/TheHeader/components/theme-switch.vue')['default']
   }
