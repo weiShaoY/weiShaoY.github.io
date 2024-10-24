971a3ff3ce999cd576603d9f2cf06e4ca0560c63
weiShaoY_ <kangwei@galaxysoftware.com.cn>
2024-10-24 12:14:41 +0800

    chore: 🔧修复全局类型

--- directives.d.ts
+++ directives.d.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import type { Directives } from 'src/directives/types'
+import type { Directives } from './src/directives/types'
 
 declare module 'vue' {
   interface ComponentCustomProperties extends Directives {}
--- src/directives.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import type { Directives } from 'src/directives/types'
-
-declare module 'vue' {
-  interface ComponentCustomProperties extends Directives {}
-}
-
