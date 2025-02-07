5e7037a52fecc88d2b5180046671ea6c69201ddd
weiShaoY_ <1604705673@qq.com>
2025-02-07 18:10:01 +0800

    chore: 🔧 调整结构

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,4 +1,9 @@
 /**
+ *  simple 简单布局组件
+ */
+export const SIMPLE_Layout = () => import('./simple/index.vue')
+
+/**
  *  home 首页模块默认布局组件
  */
 export const HOME_DEFAULT_LAYOUT = () => import('./home/index.vue')
@@ -11,9 +16,4 @@ export const BLOG_DEFAULT_LAYOUT = () => import('./blog/index.vue')
 /**
  *  blog 代码模块 iframe 渲染组件
  */
-export const BLOG_IFRAME_LAYOUT = () => import('./blog/components/iframe/index.vue')
-
-/**
- *  simple 简单布局组件
- */
-export const SIMPLE_Layout = () => import('./simple/index.vue')
+export const BLOG_IFRAME_LAYOUT = () => import('./blog/iframe/index.vue')
