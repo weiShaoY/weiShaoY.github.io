8423af391ec13d673e3d4c3ed25ac21488c625a8
weiShaoY_ <1604705673@qq.com>
2025-03-10 06:13:33 +0800

    chore: 🔧优化命名

--- src/router/modules/blog/index.ts
+++ src/router/modules/blog/index.ts
@@ -31,7 +31,7 @@ const modules = Object.fromEntries(
  * @constant
  * @description 通过调用 `formatModules` 函数格式化模块化路由，并排除当前文件，以便生成代码模块的子路由列表。
  */
-const blogChildRoutes = formatModules(modules, [])
+const blogChildRouterList = formatModules(modules, [])
 
 /**
  *  blogRouter (代码模块路由)
@@ -43,12 +43,12 @@ const blogRouter: RouteRecordRaw[] = [
     redirect: {
       name: config.blog.defaultRouteName,
     },
-    children: [...blogChildRoutes],
+    children: [...blogChildRouterList],
   },
 ]
 
 export default blogRouter
 
 export {
-  blogChildRoutes,
+  blogChildRouterList,
 }
