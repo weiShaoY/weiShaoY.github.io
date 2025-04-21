3b812026834613df1f6d6585a78e6a192099a1c1
weiShaoY_ <1604705673@qq.com>
2025-04-22 04:48:04 +0800

    chore: 🔧警务

--- src/pages/command/index.vue
+++ src/pages/command/index.vue
@@ -1,15 +1,25 @@
 <!------------------------------------    ------------------------------------------------->
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div
-    class=""
+    class="bg-[#103289] h-full w-full min-h-screen flex items-center justify-center"
   >
-    1
+    <div class="flex-col items-center gap-6">
+      <!-- 顶部 警徽和标题 -->
+      <div class="flex items-center color-white gap-2">
+        <SvgIcon icon="command-policeBadge" :size="70" />
+
+        <div class="text-8 font-bold">京城市公安局</div>
+        <div class="text-7">|</div>
+        <div class="text-6">网上协同办公平台</div>
+      </div>
+
+      <div class="h-100 w-200 bg-white rounded-2">111</div>
+
+      <div class="color-white">京城市公安局 (2025)   版权所有</div>
+    </div>
   </div>
 </template>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
