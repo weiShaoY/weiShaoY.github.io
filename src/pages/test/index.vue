da534fe61ee077f63c1ca4ce4e8844a6735d27c5
weiShaoY_ <1604705673@qq.com>
2025-04-29 18:13:22 +0800

    chore: 🔧测试

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,6 +1,14 @@
 <!------  2025-04-24---13:23---星期四  ------>
 <!------------------------------------    ------------------------------------------------->
 <script lang="ts" setup>
+import VCodeBlock from '@wdns/vue-code-block'
+
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+
+import 'prismjs/themes/prism-coy.css'
+
 const codes = `
 
 
@@ -17,13 +25,23 @@ const codes = `
 
 <template>
   <div
-    class="h-30"
+    class="h-full bg-red"
   >
+    <div
+      class="mx-auto h-100 w-100"
+    >
 
-    <PrismView
-      :codes="codes"
-      language="js"
-    />
+      <VCodeBlock
+        :code="codes"
+        class="line-numbers"
+        browser-window
+        lang="html"
+        prism-plugin
+        prismjs
+        theme="coy"
+      />
+
+    </div>
 
   </div>
 </template>
