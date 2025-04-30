9ea65dd0edd8336371f7711ee96acc3eba9ac06a
weiShaoY_ <1604705673@qq.com>
2025-04-30 09:53:40 +0800

    chore: 🔧清理代码

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,53 +1,16 @@
-<script setup lang="ts">
-
-import { html } from '@codemirror/lang-html'
-
-import { javascript } from '@codemirror/lang-javascript'
-
-import { typeScript } from '@codemirror/lang-typescript'
-
-import { dracula, solarizedLight } from 'thememirror'
-
-import CodeMirror from 'vue-codemirror6'
-
-const lang = javascript()
-
-const input = ref('')
-
+<!------  2025-04-30---09:52---星期三  ------>
+<!------------------------------------    ------------------------------------------------->
+<script lang="ts" setup>
 </script>
 
 <template>
   <div
-    class="h-screen w-full flex items-center justify-center gap-10"
+    class=""
   >
-    <el-input
-      v-model="input"
-      placeholder="请输入内容"
-    />
-
-    <div
-      class="h-200 w-200 rounded-3 bg-[rgba(171,184,195,1)] bg-auto bg-repeat p-5"
-    >
-      <CodeMirror
-        v-model="input"
-        :lang="lang"
-        :extensions="[solarizedLight]"
-        basic
-        :tab-size="4"
-        class="!h-full !w-full"
-      />
-
-    </div>
-
+    11
   </div>
-
 </template>
 
-<style>
-.cm-editor {
-  height: 100%;
-  border-radius: 12px !important;
-  border: none !important;
-  overflow: hidden;
-}
+<style lang="less" scoped>
+
 </style>
