0a843f2a95c9b669e58c50bf67877c1e9407b498
weiShao <1604705673@qq.com>
2025-08-29 21:29:42 +0800

    chore: 🔧暂存

--- src/pages/bigScreen/components/mapScene/mini3d/components/Focus.ts
+++ src/pages/bigScreen/components/mapScene/mini3d/components/Focus.ts
@@ -1,32 +1,81 @@
 import gsap from 'gsap'
-import { AdditiveBlending, Color, DoubleSide, Mesh, MeshBasicMaterial, Object3D, PlaneGeometry } from 'three'
+
+import {
+  AdditiveBlending,
+  Color,
+  DoubleSide,
+  Mesh,
+  MeshBasicMaterial,
+  Object3D,
+  PlaneGeometry,
+} from 'three'
+
 import { emptyObject } from '..'
 
+/**
+ * 焦点效果类 - 创建3D焦点动画效果
+ * 继承自Object3D，用于在地图上显示焦点动画
+ */
 export class Focus extends Object3D {
-  constructor(self, config) {
+  private config: {
+    color1: number
+    color2: number
+  }
+
+  private assets: {
+    instance: any
+  }
+
+  private sceneGroup: any
+
+  private gsapObjects: gsap.core.Tween[] // gsap动画对象数组
+
+  private animateElements: {
+    focusMidQuan: Mesh
+    focusArrows: Mesh
+    focusMoveBg: Mesh
+  }
+
+  constructor(self: any, config: any = {
+}) {
     super()
+
+    // 合并配置选项
     this.config = Object.assign(
       {
-        color1: 0xFCC957,
-        color2: 0xFFFFFF,
+        color1: 0xFCC957, // 主要颜色
+        color2: 0xFFFFFF, // 次要颜色
       },
       config,
     )
-    this.assets = { instance: null }
+
+    // 初始化资源
+    this.assets = {
+      instance: null,
+    }
     this.assets.instance = self.assets.instance
     this.sceneGroup = self.sceneGroup
-    this.gsapObjects = [] // gsap对象
-    this.animateElements = {} // gsap动画对象
+    this.gsapObjects = [] // gsap动画对象数组
+    this.animateElements = {
+    } as any // gsap动画元素对象
+
     this.init()
   }
 
+  /**
+   * 初始化焦点效果
+   */
   init() {
     const color = this.config.color1
-    // 几何体
-    const geometry = new PlaneGeometry(1.5, 1.5, 1)
-    const barGeometry = new PlaneGeometry(1, 3, 1)
+
+    // 创建几何体
+    const geometry = new PlaneGeometry(1.5, 1.5, 1) // 主要几何体
+
+    const barGeometry = new PlaneGeometry(1, 3, 1) // 条形几何体
+
     barGeometry.translate(0, 1, 0)
-    // 材质
+
+    // 创建基础材质
     const material = new MeshBasicMaterial({
       color,
       transparent: true,
@@ -34,41 +83,77 @@ export class Focus extends Object3D {
       side: DoubleSide,
       depthWrite: false,
     })
+
+    // 克隆并配置各种材质
     const focusArrowsMaterial = material.clone()
 
     focusArrowsMaterial.map = this.assets.instance.getResource('focusArrows')
+
     const focusBarMaterial = material.clone()
+
     focusBarMaterial.map = this.assets.instance.getResource('focusBar')
+
     const focusBgMaterial = material.clone()
+
     focusBgMaterial.map = this.assets.instance.getResource('focusBg')
+
     const focusMidQuanMaterial = material.clone()
+
     focusMidQuanMaterial.color = new Color(this.config.color2)
     focusMidQuanMaterial.map = this.assets.instance.getResource('focusMidQuan')
+
     const focusMoveBgMaterial = material.clone()
+
     focusMoveBgMaterial.map = this.assets.instance.getResource('focusMoveBg')
     focusMoveBgMaterial.blending = AdditiveBlending
+
+    // 创建网格对象
     const focusArrows = new Mesh(geometry, focusArrowsMaterial)
+
     const focusBar1 = new Mesh(barGeometry, focusBarMaterial)
+
     focusBar1.rotation.x = Math.PI / 2
+
     const focusBar2 = focusBar1.clone()
+
     focusBar2.rotation.y = Math.PI / 2
+
     const focusBg = new Mesh(geometry, focusBgMaterial)
+
     const focusMidQuan = new Mesh(geometry, focusMidQuanMaterial)
+
     const focusMoveBg = new Mesh(geometry, focusMoveBgMaterial)
 
+    // 将所有元素添加到组中
     const groupElement = [focusMidQuan, focusBg, focusArrows, focusMoveBg, focusBar1, focusBar2]
-    groupElement.map((element) => {
+
+    // 设置渲染顺序
+    groupElement.forEach((element) => {
       element.renderOrder = 99
     })
+
     this.add(...groupElement)
+
+    // 设置移动背景的初始缩放
     const moveBgScale = 0
+
     focusMoveBg.scale.setScalar(moveBgScale)
-    this.animateElements = { focusMidQuan, focusArrows, focusMoveBg }
+
+    // 保存需要动画的元素
+    this.animateElements = {
+      focusMidQuan,
+      focusArrows,
+      focusMoveBg,
+    }
+
     this.startAnimate()
   }
 
-  //   开始动画
+  /**
+   * 开始动画
+   */
   startAnimate() {
+    // 中间圆圈旋转动画
     const quanTween = gsap.to(this.animateElements.focusMidQuan.rotation, {
       z: 2 * Math.PI,
       duration: 8,
@@ -76,12 +161,15 @@ export class Focus extends Object3D {
       ease: 'none',
     })
 
+    // 箭头旋转动画
     const focusArrowsTween = gsap.to(this.animateElements.focusArrows.rotation, {
       z: 2 * Math.PI,
       duration: 5,
       repeat: -1,
       ease: 'none',
     })
+
+    // 移动背景缩放动画
     const focusMoveBgTween = gsap.to(this.animateElements.focusMoveBg.scale, {
       x: 1.5,
       y: 1.5,
@@ -90,28 +178,46 @@ export class Focus extends Object3D {
       repeat: -1,
       ease: 'none',
     })
+
+    // 移动背景透明度动画
     const focusMoveBgMaterialTween = gsap.to(this.animateElements.focusMoveBg.material, {
       opacity: 0,
       duration: 2.5,
       repeat: -1,
       ease: 'none',
     })
+
+    // 保存所有动画对象
     this.gsapObjects = [quanTween, focusArrowsTween, focusMoveBgTween, focusMoveBgMaterialTween]
   }
 
-  //   暂停动画
+  /**
+   * 暂停动画
+   */
   pausedAnimate() {
-    this.gsapObjects.map((element) => {
-      element.paused = true
+    this.gsapObjects.forEach((element) => {
+      ;(element as any).paused = true
     })
   }
 
+  /**
+   * 获取场景组对象
+   * @returns 场景组对象
+   */
+  getSceneGroup() {
+    return this.sceneGroup
+  }
+
+  /**
+   * 销毁焦点效果
+   */
   destroy() {
-    // 结束动画
-    this.gsapObjects.map((element) => {
+    // 结束所有动画
+    this.gsapObjects.forEach((element) => {
       element.kill()
     })
-    // 销毁
+
+    // 清空对象引用
     emptyObject(this)
   }
 }
--- src/pages/bigScreen/components/mapScene/mini3d/components/Label3d.ts
+++ src/pages/bigScreen/components/mapScene/mini3d/components/Label3d.ts
@@ -1,38 +1,84 @@
-import { CSS3DObject, CSS3DRenderer, CSS3DSprite } from 'three/examples/jsm/renderers/CSS3DRenderer'
+import {
+  CSS3DObject,
+  CSS3DRenderer,
+  CSS3DSprite,
+} from 'three/addons/renderers/CSS3DRenderer.js'
+
 import { uuid } from '../utils'
 
+/**
+ * 3D标签类 - 创建和管理CSS3D标签
+ * 用于在3D场景中显示HTML标签
+ */
 export class Label3d {
-  constructor({ scene, camera, time, sizes, canvas }) {
+  private scene: any
+  private camera: any
+  private time: any
+  private sizes: any
+  private canvas: any
+  private parent: any
+  private css3dRender: CSS3DRenderer
+
+  constructor({ scene, camera, time, sizes, canvas }: {
+    scene: any
+    camera: any
+    time: any
+    sizes: any
+    canvas: any
+  }) {
     this.scene = scene
     this.camera = camera
     this.time = time
     this.sizes = sizes
     this.canvas = canvas
     this.parent = null
+
     const { width, height } = this.sizes
+
+    // 创建CSS3D渲染器
     const css3dRender = new CSS3DRenderer()
+
     this.css3dRender = css3dRender
+
+    // 设置渲染器尺寸和样式
     css3dRender.setSize(width, height)
     css3dRender.domElement.style.position = 'absolute'
     css3dRender.domElement.style.left = '0px'
     css3dRender.domElement.style.top = '0px'
     css3dRender.domElement.style.pointerEvents = 'none'
     css3dRender.domElement.className = `label3d-${uuid()}`
+
+    // 将渲染器DOM元素添加到画布父节点
     this.canvas.parentNode.appendChild(css3dRender.domElement)
+
+    // 监听时间更新事件
     this.time.on('tick', () => {
       this.update()
     })
+
+    // 监听尺寸变化事件
     this.sizes.on('resize', () => {
       this.resize()
     })
   }
 
+  /**
+   * 创建3D标签
+   * @param content 标签内容
+   * @param className CSS类名
+   * @param isSprite 是否为精灵标签
+   * @returns 3D标签对象
+   */
   create(content = '', className = '', isSprite = false) {
+    // 创建HTML标签元素
     const tag = document.createElement('div')
+
     tag.innerHTML = content
     tag.className = className
     tag.style.visibility = 'hidden'
     tag.style.position = 'absolute'
+
+    // 如果没有指定类名，设置默认样式
     if (!className) {
       tag.style.padding = '10px'
       tag.style.color = '#fff'
@@ -41,62 +87,105 @@ export class Label3d {
       tag.style.background = 'rgba(0,0,0,0.6)'
       tag.style.borderRadius = '4px'
     }
-    let label = null
+
+    let label: any = null
+
+    // 根据类型创建不同的3D标签
     if (!isSprite) {
       label = new CSS3DObject(tag)
     }
     else {
       label = new CSS3DSprite(tag)
     }
-    label.init = (content, position) => {
+
+    // 为标签添加自定义方法
+    label.init = (content: string, position: any) => {
       label.element.innerHTML = content
       label.element.style.visibility = 'visible'
       label.position.copy(position)
     }
+
     label.hide = () => {
       label.element.style.visibility = 'hidden'
     }
+
     label.scaleHide = () => {
       label.element.classList.add('scale-hidden')
     }
+
     label.show = () => {
       label.element.style.visibility = 'visible'
       label.element.classList.remove('scale-hidden')
     }
-    label.setParent = (parent) => {
+
+    label.setParent = (parent: any) => {
       this.parent = parent
       parent.add(label)
     }
+
     label.remove = () => {
-      this.parent.remove(label)
+      if (this.parent) {
+        this.parent.remove(label)
+      }
     }
+
     return label
   }
 
-  setLabelStyle(label, scale = 0.1, axis = 'x', axisRotation = Math.PI / 2, pointerEvents = 'none') {
+  /**
+   * 设置标签样式
+   * @param label 标签对象
+   * @param scale 缩放比例
+   * @param axis 旋转轴
+   * @param axisRotation 旋转角度
+   * @param pointerEvents 指针事件
+   */
+  setLabelStyle(
+    label: any,
+    scale = 0.1,
+    axis = 'x',
+    axisRotation = Math.PI / 2,
+    pointerEvents = 'none',
+  ) {
     label.element.style.pointerEvents = pointerEvents
     label.scale.set(scale, scale, scale)
-    label.rotation[axis] = axisRotation // 控制HTML标签CSS3对象角度,
+    label.rotation[axis] = axisRotation // 控制HTML标签CSS3对象角度
   }
 
-  // 设置层级
-  setRenderLevel(zIndex) {
-    this.css3dRender.domElement.style.zIndex = zIndex
+  /**
+   * 设置渲染层级
+   * @param zIndex z-index值
+   */
+  setRenderLevel(zIndex: number) {
+    this.css3dRender.domElement.style.zIndex = zIndex.toString()
   }
 
+  /**
+   * 更新渲染
+   */
   update() {
     this.css3dRender.render(this.scene, this.camera.instance)
   }
 
+  /**
+   * 销毁标签系统
+   */
   destroy() {
     if (this.css3dRender) {
       const domElement = this.css3dRender.domElement
-      domElement.parentNode.removeChild(domElement)
+
+      if (domElement.parentNode) {
+        domElement.parentNode.removeChild(domElement)
+      }
     }
   }
 
+  /**
+   * 调整尺寸
+   */
   resize() {
     const { width, height } = this.sizes
+
     this.css3dRender.setSize(width, height)
   }
 }
